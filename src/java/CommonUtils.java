48abbb7a92ff3609fec0103a28223f68dd5ac15d
f-terehov <aybsk@yandex.ru>
2013-01-11 10:35:47 +0300

    Создан новый проект

---
+++ src/java/CommonUtils.java
@@ -0,0 +1,16 @@
+package java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: f.terehov
+ * Date: 11.01.13
+ * Time: 10:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class CommonUtils {
+    public void doAny(){
+        int i = 0;
+        int j = 2;
+        i = j;
+    }
+}
